6db798c71819d6da980839a717deb61c4ed59281
Oleg-Vaskevich <oleg_vas08@mail.ru>
2020-09-29 16:25:01 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+const humanNumbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+const tensNumbers = ['zero', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 
+                    'seventy', 'eighty', 'ninety', 'hundred', 'thousand', 'million'];
+let res = '';
+
 module.exports = function toReadable (number) {
-  
+    
+    if (number%1000000 >= 1){
+        res += humanNumbers[number[0]] + tensNumbers[12];
+        
+    } 
+    
 }
+
+
