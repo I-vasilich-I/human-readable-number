010937ce3e613082f9ec29368ccfab322ef4117e
Oleg-Vaskevich <oleg_vas08@mail.ru>
2020-10-01 16:54:00 +0300

    fix: function toReadable() almost done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 const humanNumbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-const tensNumbers = [' ', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 
+const tensNumbers = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 
                     'seventy', 'eighty', 'ninety', 'hundred', 'thousand', 'million'];
+const elevenToNinteen = ['', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
 
-//number = 526;
 module.exports = function toReadable(number) {
     let res = '';
-   // function toReadable (number) {
     let str = number.toString();
-    if(str.length==3){
-        res=humanNumbers[str[0]]+' '+tensNumbers[10]+' '
-            +tensNumbers[str[1]]+' '+humanNumbers[str[2]];
-      //  console.log(res);
-      //  console.log(str);
+    let twoDigitNumber = parseFloat(str.slice(1,3)); 
+    let oneDigitNumger = parseFloat(str.slice(2,3));
+    if (number<10) {
+        res = humanNumbers[number];
+    }
+    if (str.length==3){
+        if (twoDigitNumber<20 && twoDigitNumber>10) {
+            res=humanNumbers[str[0]]+' '+tensNumbers[10]+' '
+            +(elevenToNinteen[twoDigitNumber-10]); 
+        } else {
+            res=humanNumbers[str[0]]+' '+tensNumbers[10]+' '
+            +tensNumbers[str[1]]+(oneDigitNumger!=0 ? ' '+humanNumbers[str[2]] : '');
+        } 
+    }
+    if (str.length==2){
+        
+        
     }
     
     return res;
